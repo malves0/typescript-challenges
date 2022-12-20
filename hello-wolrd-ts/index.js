7accb2419d7405de989e3ed454a1d28d47b36a9d
Micaela Alves <micaela.alves@mercadolivre.com>
2022-12-20 15:51:07 -0300

    feat: hello wolrd

---
+++ hello-wolrd-ts/index.js
@@ -0,0 +1,8 @@
+var nome = 'John';
+var age = 30;
+var x = true;
+function greet(name, age) {
+    console.log("Hello, ".concat(name));
+    console.log("Your age is ".concat(age));
+}
+greet(nome, age);
